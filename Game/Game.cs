2c6ad2b44ba8f3fe8370bb12da8ef7c08b710231
Kaleb Hundersmarck <kalebaaron@live.com>
2017-06-15 13:14:39 -0600

    only just begun

--- Game/Game.cs
+++ Game/Game.cs
@@ -4,6 +4,21 @@ namespace CastleGrimtol.Game
 {
     public class Game : IGame
     {
+        public Room CurrentRoom {get; set;}
+        public Player CurrentPlayer{get; set;}
+        public void Setup()
+        {
+
+        }
+        public void Reset()
+        {
+
+        }
+        public void UseItem(string itemName)
+        {
+
+        }
+        
 
     }
 }
